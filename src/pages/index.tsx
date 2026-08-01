1829e54463c71f92a070f0fe587f16cc0815f27e
softgenai <admin@softgen.ai>
2026-08-01 05:04:33 +0000

    Edit /src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <>
       <SEO 
-        title="Working Bear - StubHub Arts"
-        description="Experience Working Bear on Maestro TV"
+        title="StubHub Arts"
+       
       />
       <div className="fixed inset-0 w-full h-full">
         <iframe 
           src="https://maestro.tv/working-bear?embed=theater"
           className="w-full h-full border-0"
-          title="Working Bear on Maestro TV"
+          title="StubHub Arts"
           allowFullScreen
           allow="autoplay; fullscreen; picture-in-picture"
         />
